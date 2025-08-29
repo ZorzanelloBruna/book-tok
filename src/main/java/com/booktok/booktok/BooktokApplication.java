2fc5caf9efb837d73b8eeb6493b4e2c73bf22628
Bruna Zorzanello <brunazorzanello@gmail.com>
2025-08-29 16:24:03 -0300

    primeiro commit

---
+++ src/main/java/com/booktok/booktok/BooktokApplication.java
@@ -0,0 +1,13 @@
+package com.booktok.booktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktokApplication.class, args);
+	}
+
+}
