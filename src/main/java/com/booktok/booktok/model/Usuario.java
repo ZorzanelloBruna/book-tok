e2ad70cbcfa9a0177401b024564f198bb9e1344f
Bruna Zorzanello <brunazorzanello@gmail.com>
2025-09-02 14:25:32 -0300

    ajustes de relacionamento

--- src/main/java/com/booktok/booktok/model/Usuario.java
+++ src/main/java/com/booktok/booktok/model/Usuario.java
@@ -1,10 +1,15 @@
 package com.booktok.booktok.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,6 +27,9 @@ public class Usuario {
 	
 	@Column(unique = true, nullable = false)
 	private String email;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProgressoLeitura> progressosLeitura = new ArrayList<>();
 	
 	private boolean verSpoilers;
 	
